703fbdfce72c5df95215cd8128a5a40388d79e3f
Renata Jachtoma Mendak <prana861@gmail.com>
2018-09-01 19:43:59 +0200

    next commit

---
+++ src/js/app.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+function playSound(e) {
+    const audio= document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+    audio.currentTime = 0; //start
+    audio.play();
+    key.classList.add("playing")
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return //przejdz dalej jak nie jest to transform
+    this.classList.remove('playing')
+};
+
+const keys =document.querySelectorAll(".key")
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+
+
+window.addEventListener('keydown',  playSound);
+
+});
